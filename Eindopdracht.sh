9777de1da960d20b6f7f0df217b1890e33d08e96
RPijp <r.pijp@st.hanze.nl>
2018-06-28 10:45:14 +0200

    install syslog-ng added
    And i know now how to add multiple lines to a file

--- Eindopdracht.sh
+++ Eindopdracht.sh
@@ -14,10 +14,42 @@ apt-get install cacti -y
 
 apt-get install snmp snmpd -y
 
+#####
 apt-get install cacti-spine -y
-
+#####
 
 #instal logs in de vorm van Syslog-NG
 
 sudo apt install syslog-ng -y
+
+cat <<EOT >> /etc/syslog/syslog-ng.conf
+@version: 3.5
+@include "scl.conf"
+@include "`scl-root`/system/tty10.conf"
+        options {
+                time-reap(30);
+                mark-freq(10);
+                keep-hostname(yes);
+                };
+        source s_local { system(); internal(); };
+        source s_network {
+                syslog(transport(tcp) port(514));
+                };
+        destination d_local {
+        file("/var/log/syslog-ng/messages_${HOST}"); };
+        destination d_logs {
+                file(
+                        "/var/log/syslog-ng/logs.txt"
+                        owner("rik")
+                        group("root")
+                        perm(0777)
+                        ); };
+        log { source(s_local); source(s_network); destination(d_logs); };
+EOT
+
+sudo mkdir /var/log/syslog-ng
+sudo touch /var/log/syslog-ng/logs.txt
+
+sudo systemctl start syslog-ng
+sudo systemctl enable syslog-ng
 
