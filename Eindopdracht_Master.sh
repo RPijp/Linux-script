976be61546a5d71742ceea7430a0195394020d27
RPijp <r.pijp@st.hanze.nl>
2018-06-28 13:32:18 +0200

    minion >> master 73

--- Eindopdracht_Master.sh
+++ Eindopdracht_Master.sh
@@ -70,7 +70,7 @@ sudo apt-get install salt-cloud -y
 
 sudo apt-get upgrade -y
 
-sudo systemctl restart salt-minion
+sudo systemctl restart salt-master
 
 sed -I ‘s/#interface: 0.0.0.0 /interface: 10.0.61.6/g’ /etc/salt/master
 
