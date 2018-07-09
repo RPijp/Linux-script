f841f77b4e15885b17831bf63fe5234edaf7f051
RPijp <r.pijp@st.hanze.nl>
2018-07-09 10:59:41 +0200

    change Salt installation to the front

--- Eindopdracht_Master.sh
+++ Eindopdracht_Master.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 #This is the script for the master server
 
-#instal monitoring in de form van Cacti
+#instal Salt
 
 apt-get update
 apt-get upgrade -y
+
+wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add –
+
+echo deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main >> /etc/apt/sources.list
+
+sudo apt-get update
+sudo apt-get install salt-master -y
+
+sudo apt-get upgrade -y
+
+echo interface: 10.0.61.6 >> /etc/salt/master
+
+sudo systemctl restart salt-master
+
+salt-key
+
+salt-key -A
+
+salt-master
+
+#instal monitoring in de form van Cacti
 
 apt-get install snmpd snmp apache2 libapache2-mod-php5 \
 php5-cli php5-snmp
@@ -51,23 +72,3 @@ sudo touch /var/log/syslog-ng/logs.txt
 
 sudo systemctl start syslog-ng
 sudo systemctl enable syslog-ng
-
-
-#instal saltstack master
-
-wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add –
-
-echo deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main >> /etc/apt/sources.list.d/saltstack.list
-
-#sed -I ‘s/deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main/ deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main/g’ /etc/salt/master
-
-sudo apt-get update
-sudo apt-get install salt-master -y
-
-sudo apt-get upgrade -y
-
-sudo systemctl restart salt-master
-
-sed -I ‘s/#interface: 0.0.0.0 /interface: 10.0.61.6/g’ /etc/salt/master
-
-salt-master
