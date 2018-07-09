cbad072048b4b2002834a7cfbf628a029f6c1cd2
RPijp <r.pijp@st.hanze.nl>
2018-07-09 11:22:19 +0200

    correct typo

--- Salt-Minion.sh
+++ Salt-Minion.sh
@@ -6,9 +6,9 @@
 apt-get update
 apt-get upgrade -y
 
-wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-k$
+wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add –
 
-echo deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main >> /etc/apt/sources.list$
+echo deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main >> /etc/apt/sources.list.d/saltstack.list
 
 sudo apt-get update
 sudo apt-get install salt-minion -y
@@ -20,3 +20,4 @@ echo master: 10.0.61.6 >> /etc/salt/minion
 sudo systemctl restart salt-minion
 
 salt-minion
+
