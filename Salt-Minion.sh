fbf92f12f0cc74f15ce2dffed757651e46d12b54
RPijp <r.pijp@st.hanze.nl>
2018-07-09 11:10:59 +0200

    Create salt-minion install sh

---
+++ Salt-Minion.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#This is the script for the minion server
+
+#Instal salt minion
+
+apt-get update
+apt-get upgrade -y
+
+wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-k$
+
+echo deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main >> /etc/apt/sources.list$
+
+sudo apt-get update
+sudo apt-get install salt-minion -y
+
+sudo apt-get upgrade -y
+
+echo master: 10.0.61.6 >> /etc/salt/minion
+
+sudo systemctl restart salt-minion
+
+salt-minion
