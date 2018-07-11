0970d46462b2aad99c8b70621c154814aeacbc1c
RPijp <r.pijp@st.hanze.nl>
2018-07-11 12:25:45 +0200

    Update Eindopdracht_Master.sh

--- Eindopdracht_Master.sh
+++ Eindopdracht_Master.sh
@@ -127,7 +127,12 @@ ca-certificates \
 curl \
 software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 apt-get install docker-ce -y
 
